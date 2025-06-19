63a357e755c51d5b7a5306e0478759083fb7138d
nehalpatel79 <patelneha79437@gmail.com>
2025-06-19 16:19:48 +0530

    init

---
+++ src/Container/Courty.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Courty extends Component {
+    render() {
+        return (
+            <div>
+                HELLO
+            </div>
+        );
+    }
+}
+
+export default Courty;
