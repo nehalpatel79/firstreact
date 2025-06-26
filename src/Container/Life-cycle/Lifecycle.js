209b7b4ba3a6aa193bd212d7ed7dfd425818c4c6
nehalpatel79 <patelneha79437@gmail.com>
2025-06-26 10:48:15 +0530

    init

---
+++ src/Container/Life-cycle/Lifecycle.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Lifecycle extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hi, I am a Car!</h2>;
+      </div>
+    );
+  }
+}
+
+export default Lifecycle;
